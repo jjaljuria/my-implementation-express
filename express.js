da20092bd5b57039c65e848338c54c579d75384f
jjaljuria <josejavieral13@gmail.com>
2026-01-18 23:01:58 -0400

    added middlewares functionality

--- express.js
+++ express.js
@@ -2,10 +2,16 @@ const http = require('node:http');
 
 class Express{
     stack = []
+    middlewares = []
     server = null
 
+    
     constructor(){
         this.server = http.createServer()
+    }
+    
+    use(callback){
+        this.middlewares.push(callback)
     }
 
     get(url, callback){
@@ -26,12 +32,28 @@ class Express{
     }
 
     listen(port, callback){
+
+
         this.server.on('request',(req,res)=>{
-            for(const layer of this.stack){
-                if(layer.url === req.url && layer.method === req.method){
-                    return layer.callback(req,res)
+            
+            let i = 0
+            const next = () => {
+                const middleware = this.middlewares[i++]
+                if(middleware){
+                    return middleware(req,res,next)
                 }
+
+                for(const layer of this.stack){
+                    if(layer.url === req.url && layer.method === req.method){
+                        return layer.callback(req,res)
+                    }
+                }
+
+                res.statusCode = 404
+                res.end('Not Found')
             }
+
+            next()
         })
 
         this.server.listen(port, callback)       
