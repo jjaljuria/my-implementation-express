4de9094bd355b12fe136b2cb3b6b03ee4c3e1d82
jjaljuria <josejavieral13@gmail.com>
2026-01-24 11:27:07 -0400

    converted all javascript to standard style

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const { json } = require('node:stream/consumers');
-const http = require('node:http');
-const Router = require('./lib/router');
-const Express = require('./lib/core');
+const { json } = require('node:stream/consumers')
+const http = require('node:http')
+const Router = require('./lib/router')
+const Express = require('./lib/core')
 
-function express() {
-    const router = new Router();
-    const server = http.createServer();
+function express () {
+  const router = new Router()
+  const server = http.createServer()
 
-    return new Express({server, router});
+  return new Express({ server, router })
 }
 
-express.json = function(){
-    return async function(req,res,next){
-        if(req.headers['content-type'] === 'application/json'){
-            const body = await json(req)
-            req.body = body
-        }
-        next()
+express.json = function () {
+  return async function (req, res, next) {
+    if (req.headers['content-type'] === 'application/json') {
+      const body = await json(req)
+      req.body = body
     }
+    next()
+  }
 }
 
-express.Router = function(){
-    const router = new Router();
-    return router;
+express.Router = function () {
+  const router = new Router()
+  return router
 }
 
 express.static = function (root) {
-    const { stat, createReadStream } = require('node:fs');
-    const { join} = require('node:path');
+  const { stat, createReadStream } = require('node:fs')
+  const { join } = require('node:path')
 
-    return function (req, res, next) {
-        const filePath = join(root, req.url);
-        stat(filePath, (err, stats) => {
-            if (err || !stats.isFile()) {
-                return next();
-            }
-            const stream = createReadStream(filePath);
-            stream.pipe(res);
-        });
-    };
-};
+  return function (req, res, next) {
+    const filePath = join(root, req.url)
+    stat(filePath, (err, stats) => {
+      if (err || !stats.isFile()) {
+        return next()
+      }
+      const stream = createReadStream(filePath)
+      stream.pipe(res)
+    })
+  }
+}
 
-module.exports = express;
+module.exports = express
--- test.js
+++ test.js
@@ -1,31 +1,29 @@
-const express = require('./index');
-const router = express.Router();
-const routerApi = express.Router();
+const express = require('./index')
+const router = express.Router()
+const routerApi = express.Router()
 
-const app = express();
+const app = express()
 
-app.get('/test',(req,res)=>{
-    res.send('Hello from Express-like framework!')
+app.get('/test', (req, res) => {
+  res.send('Hello from Express-like framework!')
 })
 
-router.get('/',(req,res)=>{
-    
-    res.json({message: 'Hello World!'})
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
 })
 
-router.get('/about',(req,res)=>{
-    res.json({message: 'This is the about page'})
+router.get('/about', (req, res) => {
+  res.json({ message: 'This is the about page' })
 })
 
-
-routerApi.get('/users',(req,res)=>{
-    res.json({message: 'API users'})
+routerApi.get('/users', (req, res) => {
+  res.json({ message: 'API users' })
 })
 
-app.use(router);
-app.use('/api',routerApi);
-app.use(express.json());
-app.use(express.static('public/css'));
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.use(router)
+app.use('/api', routerApi)
+app.use(express.json())
+app.use(express.static('public/css'))
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
 })
